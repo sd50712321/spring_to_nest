b1f6ff440110d302f8dc88d83be6560bcf9e63e8
SangMin <sd50712321@gmail.com>
2023-02-21 23:31:54 +0900

    디렉터리 변경에 따른 appmodule import 경로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemserviceModule } from './itemservice/itemservice.module';
+import { ItemserviceModule } from './itemservice/service/itemservice.module';
 
 @Module({
   imports: [ItemserviceModule],
