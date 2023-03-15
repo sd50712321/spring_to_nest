b3d88b5734b9fc8d316f779ad1559a094a4b0967
SangMin <sd50712321@gmail.com>
2023-03-16 00:21:40 +0900

    jest 세팅

---
+++ test/config/jest-unit.ts
@@ -0,0 +1,28 @@
+import { cpus } from 'os';
+
+const cpuCount = cpus().length;
+const maxConcurrency = cpuCount > 4 ? cpuCount - 2 : cpuCount;
+
+module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '../../src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/$1',
+    '@test/(.*)': '<rootDir>/../test/$1',
+  },
+  setupFiles: ['<rootDir>/../test/config/jest-unhandleRejection.setup.ts'],
+  setupFilesAfterEnv: ['jest-extended/all'],
+  maxConcurrency,
+  maxWorkers: maxConcurrency,
+};
