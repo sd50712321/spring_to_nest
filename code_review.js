1f40178c40e826802b2d1c838d0e61bec6f4334f
SangMin <sd50712321@gmail.com>
2023-03-16 14:08:54 +0900

    openai 수정

--- code_review.js
+++ code_review.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
-const openai = require('openai');
+const { Configuration, OpenAIApi } = require('openai');
 
 const readFile = promisify(fs.readFile);
 
-const openaiClient = new openai.OpenAi(process.env.OPENAI_API_KEY);
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 async function main() {
   const files = process.argv.slice(2);
@@ -14,16 +17,21 @@ async function main() {
   for (const file of files) {
     const code = await readFile(file, 'utf-8');
     const prompt = `Please review the following TypeScript code:\n\n${code}\n`;
-    const completions = await openaiClient.Completion.create({
-      engine: 'text-davinci-002',
-      prompt,
-      max_tokens: 150,
-      n: 1,
-      stop: ['\n'],
-      temperature: 0.7,
-    });
+    const completions = await openai.createCompletion(
+      {
+        model: 'text-davinci-003',
+        prompt,
+        max_tokens: 150,
+        n: 1,
+        stop: ['\n'],
+        temperature: 0.7,
+      },
+      {
+        timeout: 60000,
+      },
+    );
 
-    const review = completions.choices[0].text.trim();
+    const review = completions.data.choices[0].text.trim();
     reviews[path.basename(file)] = review;
   }
 
